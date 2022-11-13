f2949df735aeedbea21842b6edf6749f31e9b142
mike <michael56653556@gmail.com>
2022-11-13 00:20:08 -0500

    17:45 create interface for types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Note } from "./models/note.model";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: new Date().toString(),
+      title: "Meetings",
+      text: " Schedule meeting with UIUX team",
+      color: "#dfdfdf",
+      date: new Date().toString(),
+    },
+  ]);
+  return <div className="App">App</div>;
 }
 
 export default App;
